e365de23295ed27397e1ff30d93539bed725702b
MatthewWalters <mawalters@beambros.com>
2017-01-27 16:07:40 -0500

    Lets use async because why not

--- src/lib/message-queue.ts
+++ src/lib/message-queue.ts
@@ -10,37 +10,39 @@ export function messageLoop(messageQueue: Array<PushoverRequest>) {
     let lastRun: number = new Date().getTime();
     let consecutiveErrors: number = 0;
 
-    setInterval(() => {
+    setInterval(async() => {
+        /*
+         Send messages if the time since the last send has been at least <config.sendInterval>
+         Pushover asks that we don't abuse their API
+         */
         let now: number = new Date().getTime();
-        if (!shouldRun || now - lastRun < 5000 || messageQueue.length === 0) return false;
+        if (!shouldRun || now - lastRun < config.sendInterval || messageQueue.length === 0) return false;
 
         shouldRun = false;
         lastRun = now;
-        processMessageQueue(messageQueue)
-            .then(result => {
-                console.log(result);
-                shouldRun = true;
-                consecutiveErrors = 0;
-            })
-            .catch(err => {
-                console.error(err);
-                consecutiveErrors++;
 
-                if (consecutiveErrors >= config.maxConsecutiveErrors) {
-                    console.log('Max error limit hit, exiting');
-                    process.exit(1);
-                }
+        try {
+            let response = await processMessageQueue(messageQueue);
+            console.log(response);
+            shouldRun = true;
+            consecutiveErrors = 0;
+        } catch (err) {
+            console.error(err);
+            consecutiveErrors++;
 
-                if (err.statusCode >= 500) {
-                    messageQueue.unshift(err.options.body);
-                }
+            exitOnConsecutiveErrors(consecutiveErrors);
 
-                shouldRun = true;
-            })
+            if (err.statusCode >= 500) {
+                messageQueue.unshift(err.options.body);
+            }
+
+            shouldRun = true;
+        }
     }, config.loopInterval);
 }
 
 export function processMessageQueue(messageQueue: Array<PushoverRequest>): Promise<Array<any>> {
+    // Pushover asks that we don't abuse their API, so only process <config.maxMessagesToSend> at once
     let messages: Array<PushoverRequest> = messageQueue.splice(0, config.maxMessagesToSend);
 
     let promises: Array<RequestPromise> = messages
@@ -52,10 +54,18 @@ export function processMessageQueue(messageQueue: Array<PushoverRequest>): Promi
         .all(promises);
 }
 
-export function sendMessage(msg: PushoverRequest): RequestPromise {
+export function sendMessage(msg: PushoverRequest) {
     return rp({
         method: 'POST',
         uri: 'https://api.pushover.net/1/messages.json',
         form: msg,
     });
+}
+
+function exitOnConsecutiveErrors(consecutiveErrors: number) {
+    // If we're receiving a ton of errors, lets just quit.
+    if (consecutiveErrors >= config.maxConsecutiveErrors) {
+        console.log('Max error limit hit, exiting');
+        process.exit(1);
+    }
 }
